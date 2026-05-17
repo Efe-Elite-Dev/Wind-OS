e8475b0c803cf2a3bc9aa4f6d8dafdb84c8aa2ab
ELİTE <efe113878@gmail.com>
2026-05-17 12:21:31 +0300

    Update deb_subsystem.c

--- deb_subsystem.c
+++ deb_subsystem.c
@@ -1,6 +1,11 @@
 #include "wind_subsystem.h"
-#include "deb_subsystem.h"
+#include "deb_subsystem.h" /* Kendi başlık dosyasını dahil ederek prototipleri kilitliyoruz */
 
+/**
+ * @brief Paket Yönetimi Altyapı Rezerv Fonksiyonu
+ * İleride Sky OS / Wind OS dosya sistemi (VFS) entegre edildiğinde 
+ * içerisi .deb arşivlerini açacak mikro algoritmalarla doldurulacaktır.
+ */
 void process_deb_package(void) {
-    /* Paket Yönetimi Altyapı Rezerv Fonksiyonu */
+    /* Altyapı şu an için güvenli boş döngüde bekletiliyor */
 }
