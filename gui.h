664e167cf2ba39b6d27f91f5d1b63cb7754ebed8
ELİTE <efe113878@gmail.com>
2026-05-18 21:53:08 +0300

    Update gui.h

--- gui.h
+++ gui.h
@@ -3,14 +3,8 @@
 
 #include <stdint.h>
 
-// Temel Çizim Fonksiyonları
 void draw_rect_pure(int x, int y, int w, int h, uint8_t color);
-
-// Derleme Hatasını Çözen Güncel Pencere Fonksiyon İmzası
-// Son parametreyi tam olarak gui.c içindeki gibi 'const char* title' yapıyoruz
 void draw_window_pure(int x, int y, int w, int h, const char* title);
-
-// GUI Başlatıcı
 void gui_init(void);
 
 #endif
