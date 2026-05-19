2b054f6f22d8ce29f1f23c337273bf73ec0b75e4
ELİTE <efe113878@gmail.com>
2026-05-19 20:09:17 +0300

    Update mouse.c

--- mouse.c
+++ mouse.c
@@ -15,22 +15,24 @@ void update_cursor_position(int delta_x, int delta_y) {
     if (mouse_y >= SCREEN_HEIGHT) mouse_y = SCREEN_HEIGHT - 1;
 }
 
-void mouse_handler() {
+void mouse_handler(void) {
     static uint8_t mouse_cycle = 0;
-    static int8_t mouse_byte[3];
+    // int8_t yerine doğrudan donanımın anlayacağı signed char tipini kullanıyoruz
+    static signed char mouse_byte[3];
 
     uint8_t status = inb(0x64);
     if ((status & 0x01) && (status & 0x20)) {
-        mouse_byte[mouse_cycle++] = inb(0x60);
+        mouse_byte[mouse_cycle++] = (signed char)inb(0x60);
 
         if (mouse_cycle == 3) {
             mouse_cycle = 0;
             
-            int delta_x = mouse_byte[1];
-            int delta_y = mouse_byte[2];
+            int delta_x = (int)mouse_byte[1];
+            int delta_y = (int)mouse_byte[2];
             
             update_cursor_position(delta_x, delta_y);
             
+            // İmleci ekrana kare olarak çiz
             draw_rect(mouse_x, mouse_y, 6, 6, 0xFFFFFF);
         }
     }
