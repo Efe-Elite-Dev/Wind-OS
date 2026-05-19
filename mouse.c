ff294a44ce0fc2cf4b580647e49c2b75c45b3ecd
ELİTE <efe113878@gmail.com>
2026-05-19 20:06:26 +0300

    Update mouse.c

--- mouse.c
+++ mouse.c
@@ -7,7 +7,7 @@ static int mouse_y = 100;
 
 void update_cursor_position(int delta_x, int delta_y) {
     mouse_x += delta_x;
-    mouse_y -= delta_y; // Ekran koordinatları için ters çevrim
+    mouse_y -= delta_y; 
 
     if (mouse_x < 0) mouse_x = 0;
     if (mouse_x >= SCREEN_WIDTH) mouse_x = SCREEN_WIDTH - 1;
@@ -31,7 +31,6 @@ void mouse_handler() {
             
             update_cursor_position(delta_x, delta_y);
             
-            // Ekranda fare imlecini küçük bir kare olarak çiz
             draw_rect(mouse_x, mouse_y, 6, 6, 0xFFFFFF);
         }
     }
