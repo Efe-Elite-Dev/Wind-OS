c66a40c8cd797ff18273de23755377e3bc4169f5
ELİTE <efe113878@gmail.com>
2026-05-19 16:55:45 +0300

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,13 +1,13 @@
 #include "sky_core.h"
 
-// Global değişkenlerin ASIL tanımları burada
-uint32_t* FRAMEBUFFER = (uint32_t*)0xFD000000;
+// Header ile ismi %100 aynı olmak ZORUNDA
+uint32_t* GRAPHICS_FRAMEBUFFER = (uint32_t*)0xFD000000;
 int SCREEN_W = 1024;
 int SCREEN_H = 768;
 
 void draw_pixel(int x, int y, uint32_t color) {
-    if (FRAMEBUFFER && x >= 0 && x < SCREEN_W && y >= 0 && y < SCREEN_H) {
-        FRAMEBUFFER[y * SCREEN_W + x] = color;
+    if (GRAPHICS_FRAMEBUFFER && x >= 0 && x < SCREEN_W && y >= 0 && y < SCREEN_H) {
+        GRAPHICS_FRAMEBUFFER[y * SCREEN_W + x] = color;
     }
 }
 
@@ -33,12 +33,16 @@ void render_ui(void) {
         uint32_t color = (0xFF << 24) | (30 << 16) | (30 << 8) | 60;
         for (int j = 0; j < SCREEN_W; j++) draw_pixel(j, i, color);
     }
-    // Kart
     draw_rounded_rect(200, 150, 624, 468, 20, 0xFFFFFFFF);
 }
 
 void kernel_main(void* mboot_ptr, uint32_t magic) {
+    // Uyarıları susturmak için
+    (void)mboot_ptr;
+    (void)magic;
+
     force_graphics_hardware();
     render_ui();
+    
     while(1) { __asm__ volatile("hlt"); }
 }
