adc450c641131408e3decaefe4a44f17ab92d501
ELİTE <efe113878@gmail.com>
2026-05-19 21:03:44 +0300

    Update boot.s

--- boot.s
+++ boot.s
@@ -1,15 +1,42 @@
-; boot.s - Multiboot Header (Bunu mutlaka ekle, yoksa grafik gelmez)
+; boot.asm - Sky-Core OS Multiboot Header & Entry Point
+MULTIBOOT_ALIGN     equ 1 << 0
+MULTIBOOT_MEMINFO   equ 1 << 1
+MULTIBOOT_GRAPHICS  equ 1 << 2  ; GRUB'a grafik modu açmasını söylüyoruz
+MULTIBOOT_FLAGS     equ MULTIBOOT_ALIGN | MULTIBOOT_MEMINFO | MULTIBOOT_GRAPHICS
+MULTIBOOT_MAGIC     equ 0x1BADB002
+MULTIBOOT_CHECKSUM  equ -(MULTIBOOT_MAGIC + MULTIBOOT_FLAGS)
+
 section .multiboot
 align 4
-header_start:
-    dd 0x1BADB002             ; Magic number
-    dd 0x00010003             ; Flags: Align, Meminfo, Graphics
-    dd -(0x1BADB002 + 0x00010003) ; Checksum
+    dd MULTIBOOT_MAGIC
+    dd MULTIBOOT_FLAGS
+    dd MULTIBOOT_CHECKSUM
+    ; Grafik Ekran İstek Ayarları (800x600 32-bit renk)
+    dd 0
+    dd 800  ; Genişlik
+    dd 600  ; Yükseklik
+    dd 32   ; BPP
 
-    ; Grafik modu isteği (1024x768x32)
-    dd 0, 0, 0, 0, 0          ; Header addr
-    dd 0                      ; Mode type (0 = Linear Graphics)
-    dd 1024                   ; Width
-    dd 768                    ; Height
-    dd 32                     ; Depth
-header_end:
+section .text
+global _start
+extern kernel_main
+
+_start:
+    cli
+    mov esp, stack_top
+    
+    ; GRUB'ın bize verdiği boot parametrelerini C çekirdeğine argüman olarak paslıyoruz
+    push ebx         ; Multiboot bilgi yapısının adresi (MBI)
+    push eax         ; Sihirli numara (Magic Number)
+    
+    call kernel_main
+    
+.halt:
+    hlt
+    jmp .halt
+
+section .bss
+align 16
+stack_bottom:
+    resb 16384       ; 16KB Güvenli Stack Alanı
+stack_top:
