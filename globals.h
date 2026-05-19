bb66bd0785c8e3eef448148dc3998924665f2664
ELİTE <efe113878@gmail.com>
2026-05-19 22:23:17 +0300

    Update globals.h

--- globals.h
+++ globals.h
@@ -1,37 +1,18 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
-typedef unsigned char  uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int   uint32_t;
-typedef int            bool;
+#include <stdint.h>
+#include <stdbool.h>
 
-#define true  1
-#define false 0
-#define NULL  ((void*)0)
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 768
 
-#define SCREEN_WIDTH  800
-#define SCREEN_HEIGHT 600
+extern uint32_t* GRAPHICS_FRAMEBUFFER;
 
-// Eski 0xA0000 yerine ekran kartının gerçek fiziksel adresini tutacak küresel işaretçi
-extern uint32_t* gfx_framebuffer;
+#define gfx_framebuffer GRAPHICS_FRAMEBUFFER
 
-typedef enum {
-    STATE_WELCOME,
-    STATE_LOCATION,
-    STATE_COMPLETING,
-    STATE_DESKTOP
-} SystemState;
+extern int ai_hud_visible;
 
-typedef struct {
-    char computer_name[64];
-    char location[64];
-    char timezone[16];
-    bool wifi_connected;
-} SetupData;
-
-extern SystemState current_state;
-extern bool ai_hud_visible;
-extern SetupData os_setup_data;
+void force_graphics_hardware(void);
 
 #endif
