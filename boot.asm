35b52fe2ea6534e010fa3b1103ea48e060aab3c2
ELİTE <efe113878@gmail.com>
2026-05-20 12:58:22 +0300

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,30 +1,17 @@
 section .multiboot
 align 4
-    MULTIBOOT_MAGIC    equ 0x1BADB002
-    MULTIBOOT_FLAGS    equ 0x00000007 ; Grafik modu AKTİF edildi (0x03'tü, 0x07 yaptık)
-    MULTIBOOT_CHECKSUM equ -(MULTIBOOT_MAGIC + MULTIBOOT_FLAGS)
+    MAGIC    equ 0x1BADB002
+    FLAGS    equ 0x00000003 ; Bit 0 (Hizala) + Bit 1 (Bellek Bilgisi), Grafik Modu KAPALI
+    CHECKSUM equ -(MAGIC + FLAGS)
 
-    dd MULTIBOOT_MAGIC
-    dd MULTIBOOT_FLAGS
-    dd MULTIBOOT_CHECKSUM
-
-    ; Padding (Zorunlu)
-    dd 0 
-    dd 0 
-    dd 0 
-    dd 0 
-    dd 0 
-
-    ; Grafik Modu Çözünürlük Ayarları
-    dd 0    ; 0 = VBE Linear Framebuffer
-    dd 1024 ; Genişlik
-    dd 768  ; Yükseklik
-    dd 32   ; 32 bit renk derinliği
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
 
 section .bss
 align 16
 stack_bottom:
-    resb 16384 ; 16 KB Stack
+    resb 16384 ; Kernel için 16 KB güvenli yığın alanı
 stack_top:
 
 section .text
@@ -32,15 +19,11 @@ global _start
 extern kernel_main
 
 _start:
-    cli 
-    mov esp, stack_top 
+    cli                ; Donanım kesmelerini kapat
+    mov esp, stack_top ; Stack pointer'ı ayarla
 
-    ; GRUB'dan gelen bilgileri kernel_main(mboot_ptr, magic) içine gönder
-    push ebx ; mboot_ptr (Pointer to multiboot info)
-    push eax ; magic (0x2BADB002)
-
-    call kernel_main
+    call kernel_main   ; C koduna zıpla
 
 .hang:
-    hlt 
+    hlt
     jmp .hang
